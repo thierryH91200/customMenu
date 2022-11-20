c178e88ad0583223958b54028ad465688c81228c
thierry <thierry.hentic@gmail.com>
2022-11-20 16:06:11 +0100

    Initial Commit

---
+++ customMenu/customMenu.h
@@ -0,0 +1,18 @@
+//
+//  customMenu.h
+//  customMenu
+//
+//  Created by thierryH24 on 20/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for customMenu.
+FOUNDATION_EXPORT double customMenuVersionNumber;
+
+//! Project version string for customMenu.
+FOUNDATION_EXPORT const unsigned char customMenuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <customMenu/PublicHeader.h>
+
+
